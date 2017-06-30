843c54b61b03e6c435974dbcf1e00a4c6e858418
jbbowe <jacinta.bowe@webelevate.ie>
2017-06-30 18:21:56 +0100

    Add files via upload

---
+++ www/js/directives/todoEscape.js
@@ -0,0 +1,24 @@
+
+
+/**
+ * Directive that executes an expression when the element it is applied to gets
+ * an `escape` keydown event.
+ */
+angular.module('todomvc')//New directives are created by using the .directive function
+	.directive('todoEscape', function () {
+		'use strict';
+
+		var ESCAPE_KEY = 27;
+
+		return function (scope, elem, attrs) {
+			elem.bind('keydown', function (event) {
+				if (event.keyCode === ESCAPE_KEY) {
+					scope.$apply(attrs.todoEscape);
+				}
+			});
+
+			scope.$on('$destroy', function () {
+				elem.unbind('keydown');
+			});
+		};
+	});
